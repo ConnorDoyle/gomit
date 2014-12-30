105cd72c78940cba75c22c8a2c11580e8093324d
Nicholas Weaver <lynxbat@gmail.com>
2014-12-29 17:49:00 -0800

    more but working

--- script/test.go
+++ script/test.go
@@ -13,8 +13,7 @@ func HandleStuff(e gomit.Event) {
 	switch x := e.SourceEvent.(type) {
 	case control.ZEvent:
 		fmt.Printf("Z EVENT => name:%s & address:%s\n", x.Name, x.Address)
-	}
-case control.YEvent:
+	case control.YEvent:
 		fmt.Printf("Y EVENT => name:%s & address:%s\n", x.Name, x.Address)
 	}
 }
